16dc5ae563e4e94e65be0820e58ea29ec407f95b
Christofer Koch <cbkoch13@gmail.com>
2023-04-22 23:43:25 -0700

    Add gridGenerator()

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 // Initialize first grid
 const body = document.querySelector('body');
-const gridContainer = document.createElement('div');
+const container = document.createElement('div');
+body.appendChild(container);
+gridGenerator(16);
 
-gridContainer.classList.add('grid-container');
-body.appendChild(gridContainer);
+function gridGenerator(size) {
+    const gridContainer = document.createElement('div');
+    let row;
+    let col;
+
+    gridContainer.classList.add('grid-container');
+
+    if (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+
+    for (let i = 0; i < size; i++) {
+        row = document.createElement('div');
+        for (let j = 0; j < size; j++) {
+            col = document.createElement('div');
+            col.classList.add('square');
+            row.appendChild(col);
+        }
+        gridContainer.appendChild(row);
+    }
+    container.appendChild(gridContainer);
+}
