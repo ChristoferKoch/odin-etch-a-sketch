879e7d8084aa8d853af2edbab449bb1c9e061cf5
Christofer Koch <cbkoch13@gmail.com>
2023-04-22 22:17:51 -0700

    Initialize container

---
+++ script.js
@@ -0,0 +1,6 @@
+// Initialize first grid
+const body = document.querySelector('body');
+const gridContainer = document.createElement('div');
+
+gridContainer.classList.add('grid-container');
+body.appendChild(gridContainer);
